f5084f30dcf5c62cf7333307fc8155df450b1feb
wdcla <1499782870@qq.com>
2024-12-07 09:59:30 +0800

    feat:  查询成功

--- src/main/java/com/wdc/model/po/UserBean.java
+++ src/main/java/com/wdc/model/po/UserBean.java
@@ -1,9 +1,14 @@
 package com.wdc.model.po;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
+@TableName("s_user")
 public class UserBean {
+    @TableId(value = "uid" ,type = IdType.AUTO)
     private Integer uid;
     private String uname;
     private String uloginname;
