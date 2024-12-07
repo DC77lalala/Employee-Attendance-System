87651119d2a942e5de685ca9bb64b499798c1885
wdcla <1499782870@qq.com>
2024-12-07 10:20:02 +0800

    feat:  员工实体类等等

--- src/main/java/com/wdc/mapper/EmploymentMapper.java
+++ src/main/java/com/wdc/mapper/EmploymentMapper.java
@@ -1,11 +1,9 @@
 package com.wdc.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.wdc.model.po.UserBean;
+import com.wdc.model.po.EmploymentBean;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface UserMapper extends BaseMapper<UserBean> {
-
-
+public interface EmploymentMapper extends BaseMapper<EmploymentBean> {
 }
---
+++ src/main/java/com/wdc/model/po/EmploymentBean.java
@@ -0,0 +1,19 @@
+package com.wdc.model.po;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+@Data
+@TableName("employment")
+public class EmploymentBean {
+    @TableId(value = "eid" ,type = IdType.AUTO)
+    private Integer eid;
+    private String ename;
+    private Integer eage;
+    private String sex;
+    private String eaddr;
+    private String idcard;
+
+}
