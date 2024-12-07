0673480a90a94435d44a8a521d13bde27b131621
wdcla <1499782870@qq.com>
2024-12-07 09:02:49 +0800

    feat:项目启动成功

--- src/main/java/com/wdc/Application.java
+++ src/main/java/com/wdc/Application.java
@@ -2,10 +2,20 @@ package com.wdc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@RestController
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String test() {
+        System.out.println("hello world");
+        return "hello";
     }
 }
