9e434af09ce03772ac990e831b6bacc71e3f53d1
wdcla <1499782870@qq.com>
2024-12-12 11:29:44 +0800

    feat: 241212 定位打卡功能实现成功，接下加入 chars 图

--- src/main/java/com/wdc/controller/SignController.java
+++ src/main/java/com/wdc/controller/SignController.java
@@ -1,5 +1,35 @@
 package com.wdc.controller;
 
+
+import com.wdc.common.BaseResponse;
+import com.wdc.common.ResultUtils;
+import com.wdc.model.po.Leave;
+import com.wdc.model.po.SignIn;
+import com.wdc.service.ISignService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/sign")
+@CrossOrigin(origins = "*")
+@Slf4j
 public class SignController {
+
+    @Resource
+    private ISignService signService;
+
+    @RequestMapping(value = "/list",method = RequestMethod.GET)
+    public BaseResponse<List<SignIn>> applyLeave() {
+        List<SignIn> list = signService.list();
+        return ResultUtils.success(list);
+
+    }
+
 
 }
--- src/main/java/com/wdc/model/po/SignIn.java
+++ src/main/java/com/wdc/model/po/SignIn.java
@@ -1,48 +1,53 @@
 package com.wdc.model.po;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
+@TableName("sign_in")
 public class SignIn {
 
-
+    @TableId(value = "id" ,type = IdType.AUTO)
     private Long id;
-    /**
-     * 业务编码
-     */
-    private String buNo;
+
     /**
      * 员工编号
      */
+    @TableField(value = "employ_idcard")
     private String employIdcard;
 
     /**
      * 打卡人
      */
+    @TableField(value = "employ_name")
     private String employName;
-    /**
-     * 打卡次数
-     */
-    private Integer signCount;
+
     /**
      * 签到地点
      */
+    @TableField(value = "sign_address")
     private String signAddress;
 
 
     /**
      * 签到日期（精确到日）
      */
+    @TableField(value = "sign_in_date")
     private Date signInDate;
     /**
      * 创建时间
      */
+    @TableField(value = "create_time")
     private Date createTime;
     /**
      * 更新时间
      */
+    @TableField(value = "update_time")
     private Date updateTime;
 
 
